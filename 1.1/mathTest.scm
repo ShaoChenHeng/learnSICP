a5caf66e49c0ccb0ffe4abfd8be37d9b80453d26
shaochenheng <1010716589@qq.com>
2019-09-10 22:26:06 +0800

    add a basical math calc

--- 1.1/mathTest.scm
+++ 1.1/mathTest.scm
@@ -1,3 +1,13 @@
 ; mathTest
-(display "HelloWorld")
+(define a 4)
+(define b 5)
+; x^2
+(define (square x) (* x x))
+(square a)
+; x^2 + y^2
+(define (sumOfSquares x y) 
+    (+ (square x) (square y)) 
+)
+(sumOfSquares a b)
+
 (exit)
