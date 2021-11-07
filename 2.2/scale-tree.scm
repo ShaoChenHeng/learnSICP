7cd2b9d072d5f8be2078b9be7282bb156294f7ed
shaochenheng <1010716589@qq.com>
2021-11-07 19:19:27 +0000

    Add scale-tree scheme version.

---
+++ 2.2/scale-tree.scm
@@ -0,0 +1,16 @@
+(define (scale-tree tree factor)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (* tree factor))
+	(else (cons (scale-tree (car tree) factor)
+		    (scale-tree (cdr tree) factor)))))
+
+(define list1 (list 1 (list 2 (list 3 4) 5) (list 5 7)))
+
+(define (scale-tree1 tree factor)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	     (scale-tree sub-tree factor)
+	     (* sub-tree factor)))
+       tree))
+
+
