cf576236da1614ea045f4a1b922f50913930cb31
shaochenheng <1010716589@qq.com>
2021-11-15 18:07:50 +0000

    Complete code.

--- 2.2/2_37.scm
+++ 2.2/2_37.scm
@@ -6,14 +6,20 @@
   (accumulate + 0 (map * v w)))
 
 (define (matrix-*-vector m v)
-  (map <> m))
+  (map (lambda (col)
+	 (dot-product col v))
+       m))
 
 (define (transpose mat)
-  (accumulate-n <> <> mat))
+  (accumulate-n cons '() mat))
 
 (define (matrix-*-matrix m n)
   (let ((cols (transpose n)))
-    (map <> m)))
+    (map (lambda (col-of-m)
+	   (map (lambda (col-of-cols)
+		  (dot-product col-of-m col-of-cols))
+		cols))
+	 m)))
 
 (load "accumulate.scm")
 (load "2_36.scm")
