5f2d4d6dc532ad83c352ff3a5de2d181337e6bc3
scheng <scheng52123@gmail.com>
2021-11-18 19:14:30 +0800

    Add code.

---
+++ 2.2/2_42.scm
@@ -0,0 +1,17 @@
+(load "enumerate-interval.scm")
+
+(define (queen board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+	(list empty-board)
+	(filter
+	 (lambda (positions) (safe? k positions))
+	 (flatmap
+	  (lambda (rest-of-queens)
+	    (map (lambda (new-row)
+		   (adjoin-position new-row k rest-of-queens))
+		 (enumerate-interval 1 board-size)))
+	  (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+
