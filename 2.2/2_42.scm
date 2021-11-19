1c99714a4be811b6ca379000467d840a87a0ec3a
scheng <scheng52123@gmail.com>
2021-11-19 09:31:48 +0800

    8 queens

--- 2.2/2_42.scm
+++ 2.2/2_42.scm
@@ -1,6 +1,7 @@
 (load "enumerate-interval.scm")
+(load "prime-sum-pairs.scm")
 
-(define (queen board-size)
+(define (queens board-size)
   (define (queen-cols k)
     (if (= k 0)
 	(list empty-board)
@@ -14,4 +15,25 @@
 	  (queen-cols (- k 1))))))
   (queen-cols board-size))
 
+(define empty-board '())
 
+(define (adjoin-position new-row k rest-of-queens)
+  (cons new-row rest-of-queens))
+
+(define (safe? k position)
+  (iter-check (car position)
+	      (cdr position)
+	      1))
+
+
+(define (iter-check row-of-new-queen rest-of-queens i)
+    (if (null? rest-of-queens)
+        #t
+        (let ((row-of-current-queen (car rest-of-queens)))
+            (if (or (= row-of-new-queen row-of-current-queen)           
+                    (= row-of-new-queen (+ i row-of-current-queen))
+                    (= row-of-new-queen (- row-of-current-queen i)))
+                #f
+                (iter-check row-of-new-queen 
+                            (cdr rest-of-queens)
+                            (+ i 1))))))
