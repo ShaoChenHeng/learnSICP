562186487585c384127fcb680d58ecb59c2f2692
shaochenheng <1010716589@qq.com>
2021-11-21 20:22:19 +0000

    rename martix1->aa

--- 2.2/2_37.scm
+++ 2.2/2_37.scm
@@ -1,6 +1,6 @@
-(define matrix1 (list (list 1 2 3 4)
-		      (list 4 5 6 6)
-		      (list 6 7 8 9)))
+(define aa (list (list 1 2 3 4)
+		 (list 4 5 6 6)
+		 (list 6 7 8 9)))
 
 (define (dot-product v w)
   (accumulate + 0 (map * v w)))
