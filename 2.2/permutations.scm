6f3bc7128f5302f69c6d47ba273c04e422bfb22c
shaochenheng <1010716589@qq.com>
2021-11-22 14:41:17 +0000

    Finish permutations.scm

--- 2.2/permutations.scm
+++ 2.2/permutations.scm
@@ -1,5 +1,16 @@
+(load "filter.scm")
+(load "2_42.scm")
+
 (define (permutations s)
   (if (null? s)
       (list '())
       (flatmap (lambda (x)
-		 ))))
+		 (map (lambda (p) (cons x p))
+		      (permutations (remove x s))))
+	       s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+	  sequence))
+
+(define a (list 1 2 3 4 5))
