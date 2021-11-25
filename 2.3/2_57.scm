4d4fc7e90de4789888ac92a1ffd43d3e91f4495b
scheng <scheng52123@gmail.com>
2021-11-25 10:58:31 +0800

    finished 2.3.2

--- 2.3/2_57.scm
+++ 2.3/2_57.scm
@@ -24,25 +24,42 @@
 	(else
 	 (error "unknown expression type -- DERIV" exp))))
 
-
+(define (single-operand? lst)
+  (= (length lst)
+     1))
 
 (define (variable? x) (symbol? x))
 
 (define (same-variable? a b)
   (and (variable? a) (variable? b) (eq? a b)))
 
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-	((=number? a2 0) a1)
-	((and (number? a1) (number? a2)) (+ a1 a2))
-	(else (list '+ a1 a2))))
+(define (make-sum a1 . a2)
+  (if (single-operand? a2)
+      (let ((a2 (car a2)))
+	(cond ((=number? a1 0)
+	       a2)
+	      ((=number? a2 0)
+	       a1)
+	      ((and (number? a1) (number? a2))
+	       (+ a1 a2))
+	      (else
+	       (list '+ a1 a2))))
+      (cons '+ (conss a1 a2))))
 
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-	((=number? m1 1) m2)
-	((=number? m2 1) m1)
-	((and (number? m1) (number? m2)) (*m1 m2))
-	(else (list '* m1 m2))))
+(define (make-product m1 . m2)
+  (if (single-operand? m2)
+      (let ((m2 (car m2)))
+	(cond ((or (=number? m1 0) (=number? m2 0))
+	       0)
+	      ((=number? m1 1)
+	       m2)
+	      ((=number? m2 1)
+	       m1)
+	      ((and (number? m1) (number? m2))
+	       (* m1 m2))
+	      (else
+	       (list '* m1 m2))))
+      (cons '* (cons m1 m2))))
 
 (define (=number? exp num)
   (and (number? exp) (= exp num)))
@@ -53,8 +70,11 @@
 (define (addend x)
   (cadr x))
 
-(define (augend x)
-  (caddr x))
+(define (augend s)
+     (let ((tail-operand (cddr s)))
+       (if (single-operand? tail-operand)
+	   (car tail-operand)
+	   (apply make-sum tail-operand))))
 
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
