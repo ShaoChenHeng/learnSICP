c59d1b003e6d83206eedf20fceb10a704fbe3293
scheng <scheng52123@gmail.com>
2022-01-24 18:47:42 +0800

    Update

--- 3.1/withdraw.scm
+++ 3.1/withdraw.scm
@@ -6,3 +6,18 @@
       (begin (set! balance (- balance amount))
 	     balance)
       "Insufficient funds"))
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+	  (begin (set! balance (- balance amount))
+		 balance)
+	  "Insufficient funds"))))
+
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds")))
