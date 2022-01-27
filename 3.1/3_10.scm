b4c7a8e90ed3cc6a06984d874f3d7a19389961d4
shaochenheng <1010716589@qq.com>
2022-01-27 21:28:06 +0000

    Add 3_10.scm

---
+++ 3.1/3_10.scm
@@ -0,0 +1,9 @@
+(define make-withdraw
+  (lambda (initial-amount)
+    ((lambda (balance)
+       (lambda (amount)
+	 (if (>= balance amount)
+	     (begin (set! balance (- balance amount))
+		    balance)
+	     "Insufficient funds")))
+     initial-amount)))
